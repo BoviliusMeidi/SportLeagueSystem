d0e4cf19c34c0c630adae91b873a30e78bf83b0f
BoviliusMeidi <boviliusbm@gmail.com>
2024-06-17 23:03:14 +0700

    add teams schema

--- database/migrations/2024_06_17_150212_create_teams_table.php
+++ database/migrations/2024_06_17_150212_create_teams_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('teams', function (Blueprint $table) {
             $table->id('teamId');
+            $table->string('name');
+            $table->string('country');
+            $table->integer('founded');
             $table->timestamps();
         });
     }
