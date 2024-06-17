1130bce227d65e5c4cdb327f92acf08ee3b17c1a
BoviliusMeidi <boviliusbm@gmail.com>
2024-06-17 22:54:30 +0700

    add Models, Factories, Migrations for Team and Player

--- database/migrations/2024_06_17_150335_create_players_table.php
+++ database/migrations/2024_06_17_150335_create_players_table.php
@@ -11,20 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('players', function (Blueprint $table) {
+            $table->id('playerId');
             $table->timestamps();
-        });
-
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
         });
 
         Schema::create('sessions', function (Blueprint $table) {
@@ -42,8 +31,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('players');
         Schema::dropIfExists('sessions');
     }
 };
