484e8d47d6ee55b4083fd0507aabd2380563a913
BoviliusMeidi <boviliusbm@gmail.com>
2024-06-23 00:34:24 +0700

    add : routes for authenticationcontroller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthenticationController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -8,4 +9,12 @@ Route::get('/', function () {
 
 Route::get('/homepage', function () {
     return view('homepage');
+});
+
+Route::controller(AuthenticationController::class)->group(function () {
+    Route::get('/register', 'register')->name('register');
+    Route::post('/register', 'registerPost')->name('register.post');
+    Route::get('/login', 'login')->name('login');
+    Route::post('/login', 'loginPost')->name('login.post');
+    Route::get('/logout', 'logout')->name('logout');
 });
