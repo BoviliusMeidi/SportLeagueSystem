79dddac855dfa71f3377eb37ed49b241fcf25dab
BoviliusMeidi <boviliusbm@gmail.com>
2024-07-04 14:32:39 +0700

    change : make a template, from welcome

---
+++ resources/views/template.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" class="{{ session('theme', 'light') === 'dark' ? 'dark' : '' }}">
+@include('layouts.head')
+
+<body class="bg-primary-100 dark:bg-primary-dark">
+    @include('layouts.header')
+
+    @include('layouts.footer')
+</body>
+</html>
