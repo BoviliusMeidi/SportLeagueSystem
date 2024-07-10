f59b0fdb42c715be498f11b269e2be8b3726a805
BoviliusMeidi <boviliusbm@gmail.com>
2024-07-10 16:59:18 +0700

    add : the new route for header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -9,7 +9,7 @@
         </a>
     </div>
     <div class="flex-row items-center px-8">
-        <form action="" method="GET" class="rounded-full bg-primary-100  w-screen max-w-md flex items-center">
+        <form action="{{ route('search') }}" method="GET" class="rounded-full bg-primary-100  w-screen max-w-md flex items-center">
             <span class="material-symbols-sharp cursor-pointer text-text-100 rounded-full ml-2 py-2 px-2 ">
                 search
             </span>
@@ -20,6 +20,9 @@
     <div class="flex items-center px-8 mx-4 text-xl text-text-100">
         <a href="{{ route('homepage') }}" class="flex items-center">
             <h2 class="px-4 py-2 bg-primary-100 hover:bg-primary-200 hover:text-text-300 rounded-full mx-4">Home</h2>
+        </a>
+        <a href="{{ route('team') }}" class="flex items-center">
+            <h2 class="px-4 py-2 bg-primary-100 hover:bg-primary-200 hover:text-text-300 rounded-full mr-4">Team</h2>
         </a>
         <a href="{{ route('league') }}" class="flex items-center">
             <h2 class="px-4 py-2 bg-primary-100 hover:bg-primary-200 hover:text-text-300 rounded-full mr-4">League</h2>
